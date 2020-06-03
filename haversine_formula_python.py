13d7b5a3d7b53b21fee2526d5096cbbbbac58668
aqsakhan10 <aqsa1018@gmail.com>
2020-06-03 20:46:07 +0200

    Update haversine_formula_python.py

--- haversine_formula_python.py
+++ haversine_formula_python.py
@@ -1,22 +1,31 @@
-R = 6373.0
-radius of the Earth
+import math
 
+#Haversine function to find the distance between 2 points on a sphere by using the latitude and longitude
+def havers(lat1, lat2, lon1, lon2):
 
-lat1 = math.radians(52.2296756)
-coordinates
+    R = 6373.0
+    #radius of the Earth
 
-lon1 = math.radians(21.0122287)
-lat2 = math.radians(52.406374)
-lon2 = math.radians(16.9251681)
+    #convert the latitude longitude details to radians
+    latitude1 = math.radians(lat1)
+    longitude1 = math.radians(lon1)
+    latitude2 = math.radians(lat2)
+    longitude2 = math.radians(lon2)
 
-dlon = lon2 - lon1
-change in coordinates
+    #determine the change in coordinates
+    dlon = longitude2 - longitude1
+    dlat = latitude2 - latitude1
 
-dlat = lat2 - lat1
+    #formula to find the distance
+    a = math.sin(dlat / 2)**2 + math.cos(latitude1) * math.cos(latitude2) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = ( R * c ) * 1000
 
-a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    return(distance)
 
-c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-distance = R * c
-
-print(distance)
+lat1 = 52.467362
+lat2 = 52.466336
+lon1 = 13.320594
+lon2 = 13.328108
+result = havers(lat1, lat2, lon1, lon2)
+print(result)
