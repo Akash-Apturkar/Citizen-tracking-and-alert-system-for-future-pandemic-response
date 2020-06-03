58c976c73b9a4143aff2563b6d34ce6f7c36b2a2
nidhikulkarni21 <30890172+nidhikulkarni21@users.noreply.github.com>
2020-06-03 23:00:28 +0200

    main_function

--- main_code.py
+++ main_code.py
@@ -1,15 +1,10 @@
 from firebase import firebase
 import os.path
 import tkinter as tk
-import gmaps
-from ipywidgets.embed import embed_minimal_html
 import webbrowser
 
-
-authentication = firebase.FirebaseAuthentication('<Your API Key here>', '<Your mail id here>', extra={'id': '<Your Firebase id here>'})
-fireapp = firebase.FirebaseApplication('https://apptest-cad89.firebaseio.com/',  authentication=authentication)
-
-gmaps.configure(api_key='AIzaSyBReFmkZK6DbZomh2X9ztXRfO84HVabF3Y')
+authentication = firebase.FirebaseAuthentication('<your_api _key_here>', '<your_email_id>', extra={'id': '<your_firebase_uid>'})
+fireapp = firebase.FirebaseApplication('<your_firebase_url>',  authentication=authentication)
 
 root= tk.Tk()
 homedir = os.path.expanduser("~") #creating a home dir to store the export.html file
@@ -28,52 +23,49 @@ canvas1.create_window(350, 140, window=entry1)
 
 #root.geometry("120*60")
 
-def get_data():
+def get_data(uid):
     list1 = []
     list2 = []
     contact_persons_uid = ['001', '002','003']
     
-    for item in contact_persons_uid:
-        listbox.insert(END, item)
+    listbox = tk.Listbox(root)
     
-    firebase1 = firebase.FirebaseApplication("https://apptest-cad89.firebaseio.com/",None)
-    user_input = entry1.get()
-    label_path_lat = "/Data/{}/lat".format(user_input)
-    label_path_long = "/Data/{}/long".format(user_input)
+    for item in contact_persons_uid:
+        listbox.insert(tk.END,item)
+    
+    firebase1 = firebase.FirebaseApplication("<your_firebase_url>",None)
+    
+    label_path_lat = "/Data/{}/lat".format(uid)
+    label_path_long = "/Data/{}/long".format(uid)
     result = firebase1.get(label_path_lat,'')
     
     for i in result.split(","):
         x = float(i)
         list1.append(x)
     result = firebase1.get(label_path_long,'')
+    
     for i in result.split(","):
         x = float(i)
         list2.append(x)
         
-    locations = list(zip(list1,list2))
-    marker_locations= locations
-
-    fig=gmaps.figure()
-    markers=gmaps.marker_layer(marker_locations)
-    fig.add_layer(markers)
-    embed_minimal_html('export.html', views=[fig])
-    webbrowser.open(homedir+"/Downloads/export.html")
-
-def open():
-    x1 = entry1.get()
+    return list1, list2
+    
+def call_map():
+    uid = entry1.get()
     try:
-        get_data()
+        latitude, longitude = get_data(uid)
+        get_map(latitude, longitude)
     except Exception:
         label3['text'] = "Please check the UID"
     
+def main():
+    display_map = tk.Button(root, text = "Show Map with markers", command = call_map)
+    canvas1.create_window(350, 180, window=display_map)
+    listbox = tk.Listbox(root)
+    canvas1.create_window(350, 350, window=listbox)
+    label2 = tk.Label(root, text='Designed by \n ')
+    label2.config(font=('helvetica', 10))
+    label2.pack()
+    root.mainloop()
 
-button = tk.Button (root, text = "Show Map with markers", command = open)
-canvas1.create_window(350, 180, window=button)
-listbox = tk.Listbox(root)
-canvas1.create_window(350, 350, window=listbox)
-label2 = tk.Label(root, text='Designed by \n ')
-label2.config(font=('helvetica', 10))
-label2.pack()
-root.mainloop()
-
-
+main()
